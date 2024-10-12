b13803db6d3a530b40f8b570a360c32d6f64cf08
RanaAk <mahediAkbar02@gmail.com>
2024-10-13 02:51:13 +0600

    Initial Commit

---
+++ TuneVibe/TuneVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneVibeApp.swift
+//  TuneVibe
+//
+//  Created by RANA  on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TuneVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
